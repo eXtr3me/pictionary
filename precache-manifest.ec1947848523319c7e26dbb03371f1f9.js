8353a44100774a593b3dfb677104d7d675f85341
eXtr3me <dmfigueira@gmail.com>
2020-04-12 11:06:52 +0100

    Updates

---
+++ precache-manifest.ec1947848523319c7e26dbb03371f1f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f839060156d5ea521c7075898e4e3340",
+    "url": "/pictionary/index.html"
+  },
+  {
+    "revision": "9c38bfddc22a46bc645e",
+    "url": "/pictionary/static/css/main.5d85123f.chunk.css"
+  },
+  {
+    "revision": "1f5a0b5d46e1777f528b",
+    "url": "/pictionary/static/js/2.3c9a5109.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pictionary/static/js/2.3c9a5109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c38bfddc22a46bc645e",
+    "url": "/pictionary/static/js/main.b17c54b7.chunk.js"
+  },
+  {
+    "revision": "3b660f1b9949248f1943",
+    "url": "/pictionary/static/js/runtime-main.3e06fb15.js"
+  },
+  {
+    "revision": "8a50f1deaaee628a6c434e975e12b244",
+    "url": "/pictionary/static/media/CardFront.8a50f1de.png"
+  },
+  {
+    "revision": "9d99ccdbc28214e2a801ab8444ff5ac4",
+    "url": "/pictionary/static/media/bg_containers.9d99ccdb.jpg"
+  }
+]);
